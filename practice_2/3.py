2568c9e8874ae1bc7a1d25b29ee093fdc8c77744
AntonBash <bash_anton@mail.ru>
2021-05-19 15:33:52 +0300

    Практическое задание 3

--- practice_2/3.py
+++ practice_2/3.py
@@ -10,7 +10,12 @@ def reverse_number(n):
         return int(str(n % 10) + str(reverse_number(n // 10)))
 
 
+def reverse_number_v2(n):
+    return ''.join([i for i in str(n)[::-1]])
+
+
 if __name__ == '__main__':
     print(reverse_number(3486))
     number = int(input('Введите число: '))
     print(reverse_number(number))
+    print(reverse_number_v2(number))
